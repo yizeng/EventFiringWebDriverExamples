e56007378b6d25553cbd0937a5e2e5d6b2ae7e1f
Yi Zeng <yi.zeng.dev@gmail.com>
2014-02-07 21:49:22 +1300

    add solution

---
+++ TestOnExceptionThrown.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing.Imaging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.Events;
+using OpenQA.Selenium.Support.Extensions;
+
+namespace EventFiringWebDriverExamples {
+
+	[TestClass]
+	public class TestOnExceptionThrown {
+
+		private IWebDriver driver;
+
+		[TestInitialize]
+		public void Initialize() {
+			IWebDriver parentDriver = new FirefoxDriver();
+
+			EventFiringWebDriver eventFiringWebDriver = new EventFiringWebDriver(parentDriver);
+			eventFiringWebDriver.ExceptionThrown += new EventHandler<WebDriverExceptionEventArgs>(eventFiringWebDriver_TakeScreenshotWhenExceptionThrown);
+
+			driver = eventFiringWebDriver;
+			driver.Navigate().GoToUrl("http://stackoverflow.com");
+		}
+
+		private void eventFiringWebDriver_TakeScreenshotWhenExceptionThrown(object sender, WebDriverExceptionEventArgs e) {
+			driver.TakeScreenshot().SaveAsFile("Exception.png", ImageFormat.Png);
+		}
+
+		[TestMethod]
+		public void LoadStackOverflow() {
+			Assert.IsTrue(driver.FindElement(By.CssSelector("#hlogo > a")).Displayed);
+		}
+
+		[TestMethod]
+		public void LoadStackOverflowWithException() {
+			// Wrong locator, expect NoSuchElementException
+			Assert.IsTrue(driver.FindElement(By.CssSelector("#hlogo > a > a > a")).Displayed);
+		}
+
+		[TestCleanup]
+		public void Cleanup() {
+			driver.Quit();
+		}
+	}
+}
